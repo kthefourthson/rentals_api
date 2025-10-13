e5bf1747458d5708ff57bc9f7b3e482c944d0d71
kthefourthson <kthefourthson@gmail.com>
2025-10-13 16:41:10 +1000

    first commint for new api, all models and controllers made

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,44 @@
+class ProductsController < ApplicationController
+  def index 
+    products = Products.all
+      render json: products
+  end
+  def show 
+    product = Product.find( 
+      params[:id] 
+    )
+    render json: product
+  end 
+  def create
+    product = Product.new( 
+      name: params[:name],
+      category: params[:category],
+      rental_price: params[:rental_price]
+    )
+    if product.save 
+      render json: product
+    else 
+      render json: {errors: product.errors.full_messages}, status: :unproccessable_entity 
+    end
+  end
+    def update 
+      product = Product.find( params[:id])
+  
+      if user.update(
+        name: params[:name],
+        category: params[:category],
+        rental_price: params[:rental_price]
+      )
+        render json: product
+      
+      else 
+        render json: {errors: product.errors.full_messages}, status: :unproccessable_entity
+      end
+    end
+    def destroy 
+      product = Product.find( params[:id])
+      product.destroy 
+        render json: {message; "product deleted"},
+        status: :ok 
+    end
+end
