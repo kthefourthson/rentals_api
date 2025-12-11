a0f1b34bb55898ba16d3ab9de3eb4420fd6668b9
kthefourthson <kthefourthson@gmail.com>
2025-12-11 21:07:27 +1000

    fixed product name

--- db/migrate/20251210081338_create_product_rentals.rb
+++ db/migrate/20251210081338_create_product_rentals.rb
@@ -2,7 +2,7 @@ class CreateProductRentals < ActiveRecord::Migration[8.0]
   def change
     create_table :product_rentals do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :car, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
       t.date :start_date
       t.date :end_date
       t.decimal :total_price
